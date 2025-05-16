7a87e260b17abd1407108c98d543971636d71c6b
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-05-16 08:04:25 +0700

    membuat class node

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node {
+    public:
+        int rollNo;
+        string name;
+        Node* prev;
+        Node* next;
+    };
