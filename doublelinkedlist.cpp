7074de648595105ebd22e199d81fee6383a2d00e
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-05-16 08:07:07 +0700

    membuat prosedur

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -11,11 +11,28 @@ public:
     Node *next;
 };
 
-class DoubleLinkedList {
-    private:
-        Node* START;
-    
-    public:
-        DoubleLinkedList() {
-            START = NULL;
-        }
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+    void addNode()
+    {
+        int rollNo;
+        string nm;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> rollNo;
+        cout << "\nEnter the name of the student: ";
+        cin.ignore();
+        getline(cin, nm);
+
+        Node *newNode = new Node();
+        newNode->rollNo = rollNo;
+        newNode->name = nm;
+        newNode->prev = NULL;
+        newNode->next = NULL;
